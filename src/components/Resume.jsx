13f812c723f02c19c3cecde0bdc2a34dd928d7e7
single-byte <joharkhan1999@gmail.com>
2024-02-21 16:39:58 +0500

    Update style-dark.css with color and style changes

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,220 +1,228 @@
 import React from "react";
 
 const Resume = () => {
+  const Experience = [
+    {
+      title: "Full Stack Developer",
+      company: "Freelancer",
+      location: "Remote",
+      date: "12/2021 - Present",
+      description: `I participated in the development of various projects with a team.
+      Developed websites using various frameworks and programming
+      technologies, including PHP, MySQL, CSS/HTML, Javascript, Databases,
+      JQuery, React, Node.js/Express, CodeIgniter, and Laravel.
+      Hands-on experience using cPanel, phpMyAdmin, DNS records, and other
+      various aspects of web servers such as Addon domains, email, FTP, and SSL.
+      Hired and directed multiple full-stack developers, working to establish key
+      systems in the business.`,
+    },
+    {
+      title: "Data Science Intern",
+      company: "Glowing Soft Technologies",
+      location: "Remote",
+      date: "07/2023 - 09/2023",
+      description: `Engaging in real-world data science projects, honing my data visualisation,
+      statistical analysis, and analytical problem-solving expertise.
+      Collaborating with seasoned data professionals and gaining insights into
+      industry best practices and emerging trends in data science.
+      Using Python and different libraries to develop data applications and
+      solutions that provide actionable insights.`,
+    },
+    {
+      title: "Full Stack Developer",
+      company: "Kaspar Consulting Services",
+      location: "Remote",
+      date: "02/2022 - 11/2022",
+      description: `Collaborating with 3+ agencies, we developed websites for clients in the food
+      delivery, entertainment, and logistics industries.
+      I worked on TELA software. A telecommunication product to benchmark
+      network data in South Africa and Pakistan.
+      Developed full-stack web applications that processed, analysed, and
+      rendered data visually.`,
+    },
+    {
+      title: "Web Developer",
+      company: "Constantine PR & Communications",
+      location: "Islamabad, Pakistan",
+      date: "08/2021 - 03/2022",
+      description: `Designed and optimized user-friendly applications, boosting website
+      conversion rates and customer satisfaction.
+      I worked with a team of four web developers to develop innovative systems
+      that will help each section save
+      both man-hours and manpower.
+      Mobilized and optimized client applications for compatibility across devices,
+      ensuring exceptional performance.`,
+    },
+    {
+      title: "Web Developer",
+      company: "Infology Solutions",
+      location: "Islamabad, Pakistan",
+      date: "02/2021 - 08/2021",
+      description: `70% of increment in skills like computer maintenance, web application
+      development, web design and increased my programming skills.
+      Created and maintained 5+ web applications for numerous national and
+      foreign clients.
+      Facilitated launch of new corporate website, increasing web traffic by 32%
+      over year prior.
+      Utilised creativity and technical expertise to craft simple and fun games.`,
+    },
+  ];
+
+  const Education = [
+    {
+      title: "Bs. Software Engineering",
+      company: "Comsats University",
+      location: "Islamabad, Pakistan",
+      date: "09/2019 - 01/2024",
+    },
+  ];
+
+  const Skills = [
+    {
+      title: "Front-End Languages",
+      skills: [
+        "JavaScript",
+        "React",
+        "HTML/CSS",
+        "Bootstrap",
+        "Tailwind",
+        "Vue JS",
+        "JQuery",
+        "Next JS",
+        "TypeScript",
+      ],
+    },
+    {
+      title: "Backend Languages",
+      skills: [
+        "PHP",
+        "Node JS",
+        "Express JS",
+        "Laravel",
+        "Python",
+        "C#",
+        "JAVA",
+        "AWS",
+        "Spring Boot (Beginner)",
+        "Solidity",
+        "JUnit (Beginner)",
+        "Jenkins",
+      ],
+    },
+    {
+      title: "Mobile App Development",
+      skills: ["React Native", "Expo", "Flutter", "Dart", "Nativewind"],
+    },
+    {
+      title: "Databases",
+      skills: [
+        "MYSQL",
+        "MongoDB",
+        "Firebase",
+        "PostgreSQL",
+        "IPFS",
+        "Cloudinary",
+      ],
+    },
+    {
+      title: "CMS Systems",
+      skills: ["WordPress", "Webflow"],
+    },
+    {
+      title: "Tools",
+      skills: [
+        "Unity",
+        "Github/Git",
+        "Jupyter Notebook",
+        "JIRA/Trello",
+        "Postman",
+        "NPM",
+        "Android Studio",
+        "VS Code",
+      ],
+    },
+  ];
+
   return (
     <div class="page pt-resume" data-simplebar>
       <section class="container">
-        {/* <!-- Section Title --> */}
         <div class="header-page mt-70 mob-mt">
           <h2>Resume</h2>
           <span></span>
         </div>
 
-        {/* <!-- Experience & Education Row Start --> */}
         <div class="row mt-100">
-          {/* <!-- Experience Column Start --> */}
           <div class="col-lg-6 col-sm-12">
-            {/* <!-- Header Block --> */}
             <div class="header-box mb-50">
               <h3>Experience</h3>
             </div>
 
             <div class="experience box-1">
-              {/* <!-- Experience Item --> */}
-              <div class="item">
-                <div class="main">
-                  <h4>Web Developer</h4>
-                  <div class="d-flex justify-content-between mb-4 align-content-center">
-                    <p class="m-0">
-                      Kaspar Consulting Services <br /> Remote
-                    </p>
-                    <p class="m-0">
-                      <i class="far fa-calendar-alt"></i>Feb 2022 - Ongoing
-                    </p>
+              {Experience.map((exp, index) => (
+                <div class="item" key={index}>
+                  <div class="main">
+                    <h4>{exp.title}</h4>
+                    <div class="d-flex justify-content-between mb-4 align-content-center">
+                      <p class="m-0">
+                        {exp.company} <br />
+                        {exp.location}
+                      </p>
+                      <p class="m-0">
+                        <i class="far fa-calendar-alt"></i>
+                        {exp.date}
+                      </p>
+                    </div>
                   </div>
+                  <p>{exp.description}</p>
                 </div>
-                <p>
-                  Contributed to the development of over 3 web and mobile
-                  applications Worked with group of developers to develop and
-                  maintain telecommunication product. Created and designed
-                  webflow applications. Interact with the designer to suggest
-                  changes and to make sure the view he has about the design is
-                  translated into actual functionality.
-                </p>
-              </div>
-
-              {/* <!-- Experience Item --> */}
-              <div class="item">
-                <div class="main">
-                  <h4>Full Stack Developer</h4>
-                  <div class="d-flex justify-content-between mb-4 align-content-center">
-                    <p class="m-0">
-                      Constantine PR & Communications <br />
-                      Hybrid
-                    </p>
-                    <p class="m-0">
-                      <i class="far fa-calendar-alt"></i>Sep 2021 - Feb 2022
-                    </p>
-                  </div>
-                </div>
-                <p>
-                  Designed and developed user-friendly website, including
-                  optimized check-out page. Mobilized and optimized web sites.
-                  Fixed bugs from existing websites and implemented enhancements
-                  that significantly improved web functionality and speed.
-                </p>
-              </div>
-
-              {/* <!-- Experience Item --> */}
-              <div class="item">
-                <div class="main">
-                  <h4>Fullstack Developer</h4>
-                  <div class="d-flex justify-content-between mb-4 align-content-center">
-                    <p class="m-0">
-                      Infology Solutions <br />
-                      Onsite
-                    </p>
-                    <p class="m-0">
-                      <i class="far fa-calendar-alt"></i>Jan 2021 - Sep 2021
-                    </p>
-                  </div>
-                </div>
-                <p>
-                  Developed full-stack applications across various platforms
-                  using latest industry-adopted technologies and frameworks.
-                  Played a key role in the development, improvement, and
-                  operation of web-based software and games. Worked here to
-                  build lot of static and dynamic web sites/apps. Created simple
-                  and fun games.
-                </p>
-              </div>
-
-              {/* <!-- Experience Item --> */}
-              <div class="item">
-                <div class="main">
-                  <h4>Fullstack Developer</h4>
-                  <div class="d-flex justify-content-between mb-4 align-content-center">
-                    <p class="m-0">
-                      Freelance <br />
-                      Remote
-                    </p>
-                    <p class="m-0">
-                      <i class="far fa-calendar-alt"></i>2019 - 2023
-                    </p>
-                  </div>
-                </div>
-                <p>
-                  Use my extensive experience with front end development to
-                  define the structure and components for the project, making
-                  sure they are reusable. Keep the code quality high reviewing
-                  code from other developers and suggesting improvements.
-                  Interact with the designer to suggest changes and to make sure
-                  the view he has about the design is translated into actual
-                  functionality.
-                </p>
-              </div>
+              ))}
             </div>
           </div>
 
-          {/* <!-- Education Column Start --> */}
           <div class="col-lg-6 col-sm-12">
-            {/* <!-- Header Block --> */}
-            <div class="header-box mb-50 mob-box-mt">
-              <h3>Education</h3>
-            </div>
-
-            <div class="experience box-2">
-              {/* <!-- Education Item --> */}
-              <div class="item">
-                <div class="main">
-                  <h4>Bs. Software Engineering</h4>
-                  <div class="d-flex justify-content-between mb-4 align-content-center">
-                    <p class="m-0">
-                      Comsats University <br />
-                      Islamabad, Pakistan
-                    </p>
-                    <p class="m-0">
-                      <i class="far fa-calendar-alt"></i>2019 - Ongoing
-                    </p>
-                  </div>
-                </div>
-                <p>
-                  A bachelor is a man who comes to work each morning from a
-                  different direction.
-                </p>
+            <div>
+              <div class="header-box mb-50 mob-box-mt">
+                <h3>Education</h3>
               </div>
-
-              {/* <!-- Education Item --> */}
-              <div class="item">
-                <div class="main">
-                  <h4>High School (Pre-Engineering)</h4>
-                  <div class="d-flex justify-content-between mb-4 align-content-center">
-                    <p class="m-0">
-                      PSCCB <br />
-                      Mansehra, Pakistan
-                    </p>
-                    <p class="m-0">
-                      <i class="far fa-calendar-alt"></i>2014 - 2018
-                    </p>
+              <div class="experience box-2">
+                {Education.map((edu, index) => (
+                  <div class="item" key={index}>
+                    <div class="main">
+                      <h4>{edu.title}</h4>
+                      <div class="d-flex justify-content-between mb-4 align-content-center">
+                        <p class="m-0">
+                          {edu.company} <br />
+                          {edu.location}
+                        </p>
+                        <p class="m-0">
+                          <i class="far fa-calendar-alt"></i>
+                          {edu.date}
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                </div>
+                ))}
               </div>
             </div>
-          </div>
-        </div>
 
-        {/* <!-- Skills Row Start --> */}
-        <div class="row mt-100">
-          {/* <!-- Header Block --> */}
-          <div class="col-md-12">
-            <div class="header-box mb-50">
-              <h3>Skills</h3>
-            </div>
-          </div>
-        </div>
-
-        <div class="box-1 skills mb-100">
-          <div class="row">
-            <div class="col-lg-12 col-sm-12">
-              <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>Javascript</li>
-                <li>PHP</li>
-                <li>Bootstrap</li>
-                <li>Jquery</li>
-                <li>Skeleton CSS</li>
-                <li>Node JS</li>
-                <li>WEB RTC</li>
-                <li>JAVA</li>
-                <li>Python</li>
-                <li>C++</li>
-                <li>Wordpress</li>
-                <li>Shopify</li>
-                <li>SQL</li>
-                <li>Unity</li>
-                <li>Laravel</li>
-                <li>JSP</li>
-                <li>Android Studio</li>
-                <li>AJAX</li>
-                <li>Git</li>
-                <li>NPM</li>
-                <li>API</li>
-                <li>MYSQL</li>
-                <li>SQLite</li>
-                <li>JSON</li>
-                <li>Mongodb</li>
-                <li>Oracledb</li>
-                <li>Postgresql</li>
-                <li>C#</li>
-                <li>PUG</li>
-                <li>Parse Server</li>
-                <li>Materialize CSS</li>
-                <li>Wix</li>
-                <li>React</li>
-                <li>SASS</li>
-                <li>Heroku</li>
-              </ul>
+            <div className="mt-50 skills">
+              <div class="header-box mb-50 mob-box-mt">
+                <h3>Skills</h3>
+              </div>
+              <div class="experience box-2">
+                {Skills.map((skill, index) => (
+                  <div class="item" key={index}>
+                    <div class="main">
+                      <h4>{skill.title}</h4>
+                      <ul>
+                        {skill.skills.map((s, index) => (
+                          <li key={index}>{s}</li>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
