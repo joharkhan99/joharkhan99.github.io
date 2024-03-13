56b96af517c4a90b39c4ae3fa5a07dd9d6f5b4ac
single-byte <joharkhan1999@gmail.com>
2024-03-13 16:05:19 +0500

    Added bento grids

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -3,173 +3,207 @@ import { Boxes, BoxesCore } from "./aceternity/bg-boxes.tsx";
 import { ParallaxScroll } from "./aceternity/parallax-scroll.tsx";
 import { LayoutGrid } from "./aceternity/layout-grid.tsx";
 import { DirectionAwareHover } from "./aceternity/direction-aware.tsx";
-import { GoArrowUpRight } from "react-icons/go";
+import { BentoGrid, BentoGridItem } from "./aceternity/bento-grid.tsx";
 
 const Portfolio = () => {
   const portfolioItems = [
     {
-      id: 1,
       title: "QuickCal: Calendar & Events App",
       image: require("../assets/portfolio/quickcal.png"),
       link: "https://play.google.com/store/apps/details?id=com.jka.quickcal",
-      className: "md:col-span-2",
+      description:
+        "QuickCal is a calendar and events app that allows you to create and manage events, set reminders, and view your schedule in a monthly, weekly, or daily view.",
+      // className: "md:col-span-2",
     },
     {
-      id: 2,
       title: "NFLUENCER Website",
       image: require("../assets/portfolio/nfluencermock.png"),
       link: "https://nfluencer-website.vercel.app/",
-      className: "col-span-1",
+      description:
+        "NFLUENCER is a digital platform that allows influencers, freelancers, musicians, artists, consultants, and coaches to create and sell gigs that offer their services to buyers.",
+      // className: "col-span-1",
     },
     {
-      id: 3,
       title: "NFLUENCER Mobile App",
       image: require("../assets/portfolio/nfluencermobile1.png"),
       link: "https://play.google.com/store/apps/details?id=com.joharkhan051.nfluencer&pcampaignid=web_share",
-      className: "col-span-1",
+      description:
+        "NFLUENCER is a digital platform that allows influencers, freelancers, musicians, artists, consultants, and coaches to create and sell gigs that offer their services to buyers.",
+      // className: "col-span-1",
     },
     {
-      id: 4,
       title: "Less More Japan",
       image: require("../assets/portfolio/lessmore.png"),
       link: "https://less-more-japan.vercel.app/",
-      className: "md:col-span-2",
+      description:
+        "Platform for generating time and price estimates for corporate websites and business systems.",
+      // className: "md:col-span-2",
     },
     {
-      id: 5,
       title: "MovieLand: Free Movie Streaming",
       image: require("../assets/portfolio/movieland.png"),
       link: "https://joharkhan.itch.io/movieland",
-      className: "col-span-2",
+      description:
+        "MovieLand is a free movie streaming app that allows you to watch movies and TV shows for free.",
+      // className: "col-span-2",
     },
     {
-      id: 6,
       title: "Bueno Express Transport",
       image: require("../assets/portfolio/bueno.png"),
       link: "https://www.buenoexpresstransport.com/",
-      className: "col-span-1",
+      description:
+        "Bueno Express Transport is a transportation company that provides transportation services to the public.",
+      // className: "col-span-1",
     },
     {
-      id: 7,
       title: "LYEL Accountants",
       image: require("../assets/portfolio/lyel.png"),
       link: "https://lyel.co.uk/",
-      className: "col-span-1",
+      description:
+        "LYEL Accountants is a firm of Chartered Certified Accountants based in London, UK.",
+      // className: "col-span-1",
     },
     {
-      id: 8,
       title: "TELA",
       image: require("../assets/portfolio/tela.png"),
       link: "#",
-      className: "md:col-span-2",
+      description:
+        "Telecommunication based product to benchmark and monitor the performance of the network and the quality of service (QoS) provided to the end-users.",
+      // className: "md:col-span-2",
     },
     {
-      id: 9,
       title: "Postlink POS System",
       image: require("../assets/portfolio/pos.png"),
       link: "https://postlink-erp.co.za/postlink/",
       status: "in development",
-      className: "col-span-2",
+      description:
+        "Postlink is a point of sale system that allows you to manage your business, sales, and inventory.",
+      // className: "col-span-2",
     },
     {
-      id: 10,
       title: "Likhdy.com",
       image: require("../assets/portfolio/likhdy.png"),
       link: "https://likhdy.com/",
-      className: "col-span-1",
+      description:
+        "Likhdy is a platform for writers, poets, and authors to write and publish their work.",
+      // className: "col-span-1",
     },
     {
-      id: 11,
-      title: "Savour Foods",
-      image: require("../assets/portfolio/savour.png"),
-      link: "http://bruh.ezyro.com/savour-foods/",
-      className: "col-span-1",
-    },
-    {
-      id: 12,
-      title: "QWQER",
-      image: require("../assets/portfolio/qwqer.png"),
-      link: "https://qwqer.pk/",
-      className: "md:col-span-2",
-    },
-    {
-      id: 13,
-
       title: "Tiffin Box",
       image: require("../assets/portfolio/tiffin.png"),
       link: "https://www.tiffinboxcompany.com/",
-      className: "col-span-2",
+      description:
+        "Tiffin Box is a food delivery service that delivers food to your doorstep.",
+      // className: "col-span-2",
+    },
+    {
+      title: "Savour Foods",
+      image: require("../assets/portfolio/savour.png"),
+      link: "http://bruh.ezyro.com/savour-foods/",
+      description:
+        "Savour Foods is a food delivery service that delivers food to your doorstep.",
+      // className: "col-span-1",
+    },
+    {
+      title: "QWQER",
+      image: require("../assets/portfolio/qwqer.png"),
+      link: "https://qwqer.pk/",
+      description:
+        "QWQER is a platform for ordering food, groceries, and other items.",
+      // className: "md:col-span-2",
     },
 
     {
-      id: 14,
       title: "Bullion VTU",
       image: require("../assets/portfolio/bullion.png"),
       link: "https://bullionvtu.com/",
-      className: "col-span-1",
+      description:
+        "Bullion VTU is a platform for buying and selling digital currencies.",
+      // className: "col-span-1",
     },
     {
-      id: 15,
       title: "BME Real Estate",
       image: require("../assets/portfolio/bme.png"),
       link: "https://beaumonde-estate.com/",
-      className: "col-span-1",
+      description:
+        "BME Real Estate is a real estate company that provides real estate services to the public.",
+      // className: "col-span-1",
     },
     {
-      id: 16,
       title: "Winston Investment",
       image: require("../assets/portfolio/winstoninvestment.png"),
       link: "https://winstoninvestment.co.uk",
-      className: "md:col-span-2",
+      description:
+        "Winston Investment is a real estate company that provides real estate services to the public.",
+      // className: "md:col-span-2",
     },
     {
-      id: 17,
       title: "InSol",
       image: require("../assets/portfolio/insol.png"),
       link: "https://infologysolutions.com/",
-      className: "col-span-2",
+      description:
+        "Corporate website for InSol, a software development company.",
+      // className: "col-span-2",
     },
     {
-      id: 18,
       title: "ZigZag Ball 3D Game",
       image: require("../assets/portfolio/zigzag.webp"),
       link: "https://play.google.com/store/apps/details?id=com.ActivGames.ZigZagBall3D",
-      className: "col-span-1",
+      description:
+        "ZigZag Ball 3D is a 3D game that allows you to control a ball and avoid obstacles.",
+      // className: "col-span-1",
     },
     {
-      id: 19,
       title: "DayCare",
       image: require("../assets/portfolio/daycare.png"),
       link: "https://hellokiddiesdaycare.com/",
       status: "closed",
-      className: "col-span-1",
+      description:
+        "DayCare is a platform for parents to find and book daycare services for their children.",
+      // className: "col-span-1",
     },
     {
-      id: 20,
       title: "Khyrie Allyene",
       image: require("../assets/portfolio/khyrie.png"),
       link: "http://khyriealleyne.com/",
       status: "closed",
-      className: "md:col-span-2",
+      description:
+        "Khyrie Allyene is a platform for Khyrie Allyene to showcase his work and services.",
+      // className: "md:col-span-2",
     },
     {
-      id: 21,
       title: "Music Player Web App",
       image: require("../assets/portfolio/music.png"),
       link: "http://bruh.ezyro.com/spotifyclone",
       status: "closed",
-      className: "md:col-span-2",
+      description:
+        "Music Player is a web app that allows you to listen to music and create playlists.",
+      // className: "md:col-span-2",
     },
   ];
 
   return (
     <div id="portfolio" className="relative py-6 pt-14 h-full">
-      <div className="container mx-auto px-8">
+      <div className="container mx-auto px-8 relative">
         <div className="relative z-10 text-5xl md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 to-neutral-500  text-center font-sans font-bold">
           Portfolio
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 mt-14 place-items-center">
+        <BentoGrid className="mx-auto mt-14 z-50">
+          {portfolioItems.map((item, i) => (
+            <BentoGridItem
+              key={i}
+              item={item}
+              className={
+                i === 3 || i === 6 || i === 10 || i === 16 || i === 17
+                  ? "md:col-span-2"
+                  : ""
+              }
+            />
+          ))}
+        </BentoGrid>
+
+        {/* <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 mt-14 place-items-center">
           {portfolioItems.map((item, index) => (
             <DirectionAwareHover imageUrl={item.image} key={index}>
               <p className="font-bold text-xl">{item.title}</p>
@@ -188,7 +222,7 @@ const Portfolio = () => {
               </p>
             </DirectionAwareHover>
           ))}
-        </div>
+        </div> */}
       </div>
 
       {/* <LayoutGrid cards={portfolioItems} /> */}
