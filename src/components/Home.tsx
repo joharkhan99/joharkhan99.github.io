0867c264be9739ef27ce54809fbda6f70bab5a5a
single-byte <joharkhan1999@gmail.com>
2024-03-24 20:08:53 +0500

    updated spotlight

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -31,59 +31,64 @@ const Home = ({
 }) => {
   return (
     <div className="relative" id="home">
-      <div
-        className={cn(
-          "h-screen flex flex-col items-center justify-center",
-          containerClassName
-        )}
-      >
-        <SpotLight
-          className="-top-40 left-0 md:left-60 md:-top-20"
-          fill="white"
-        />
+      <div className="h-full w-full dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative flex items-center justify-center">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
-        <div className={cn("z-10", className)} {...props}>
-          <div className="absolute sm:top-5 top-5 right-5 hidden sm:block">
-            <div className="flex flex-col gap-5">
-              <a
-                href="https://www.linkedin.com/in/johar-khan"
-                className="hover:text-white text-[#ccc] transition-colors duration-200"
-              >
-                <FaLinkedin color="inherit" size={18} />
-              </a>
-              <a
-                href="https://github.com/joharkhan99"
-                className="hover:text-white text-[#ccc] transition-colors duration-200"
-              >
-                <FaGithub size={18} />
-              </a>
-              <a
-                href="mailto:joharkhan1999@gmail.com"
-                className="hover:text-white text-[#ccc] transition-colors duration-200"
-              >
-                <MdAlternateEmail size={18} />
-              </a>
+        <div
+          className={cn(
+            "h-screen flex flex-col items-center justify-center",
+            containerClassName
+          )}
+        >
+          <SpotLight
+            className="top-0 left-0 md:left-60 md:-top-20"
+            fill="white"
+          />
+
+          <div className={cn("z-10", className)} {...props}>
+            <div className="absolute sm:top-5 top-5 right-5 hidden sm:block">
+              <div className="flex flex-col gap-5">
+                <a
+                  href="https://www.linkedin.com/in/johar-khan"
+                  className="hover:text-white text-[#ccc] transition-colors duration-200"
+                >
+                  <FaLinkedin color="inherit" size={18} />
+                </a>
+                <a
+                  href="https://github.com/joharkhan99"
+                  className="hover:text-white text-[#ccc] transition-colors duration-200"
+                >
+                  <FaGithub size={18} />
+                </a>
+                <a
+                  href="mailto:joharkhan1999@gmail.com"
+                  className="hover:text-white text-[#ccc] transition-colors duration-200"
+                >
+                  <MdAlternateEmail size={18} />
+                </a>
+              </div>
             </div>
-          </div>
 
-          <div className="flex items-center flex-col gap-1">
-            <p className="text-5xl lg:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 inter-var text-center">
-              Hi, I'm Johar Khan
-            </p>
-            <p className="sm:text-lg text-base mt-4 text-white font-normal inter-var text-center">
-              I'm a Software Developer on a journey to turn ideas into reality.
-            </p>
-            <button
-              className="backdrop-blur-lg mt-5 p-3 px-5 bg-white bg-opacity-10 rounded-full hover:bg-white hover:text-black text-white font-semibold text-sm transition-all duration-500 flex items-center gap-2 active:scale-90"
-              onClick={() => {
-                document
-                  .getElementById("contact")
-                  ?.scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              <span>Get in Touch</span>
-              <GoChevronRight size={20} />
-            </button>
+            <div className="flex items-center flex-col gap-1">
+              <p className="text-5xl lg:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 inter-var text-center">
+                Hi, I'm Johar Khan
+              </p>
+              <p className="sm:text-lg text-base mt-4 text-white font-normal inter-var text-center">
+                I'm a Software Developer on a journey to turn ideas into
+                reality.
+              </p>
+              <button
+                className="backdrop-blur-lg mt-5 p-3 px-5 bg-white bg-opacity-10 rounded-full hover:bg-white hover:text-black text-white font-semibold text-sm transition-all duration-500 flex items-center gap-2 active:scale-90"
+                onClick={() => {
+                  document
+                    .getElementById("contact")
+                    ?.scrollIntoView({ behavior: "smooth" });
+                }}
+              >
+                <span>Get in Touch</span>
+                <GoChevronRight size={20} />
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/components/aceternity/spotlight.tsx
+++ src/components/aceternity/spotlight.tsx
@@ -10,7 +10,7 @@ export const SpotLight = ({ className, fill }: SpotlightProps) => {
   return (
     <svg
       className={cn(
-        "animate-spotlight pointer-events-none absolute z-[1]  h-[169%] w-[138%] lg:w-[84%] opacity-0",
+        "animate-spotlight pointer-events-none absolute z-[1]  h-[169%] md:w-[138%] w-[200%] lg:w-[84%] opacity-0",
         className
       )}
       xmlns="http://www.w3.org/2000/svg"
